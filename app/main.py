0715ca76675ff665c93614f50570d61b06bbd1d6
Shivakoreddi <>
2021-11-02 00:15:58 -0400

    commit

--- app/main.py
+++ app/main.py
@@ -1,8 +1,13 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
+import pymongo as pm
 app= Flask(__name__)
+client = pm.MongoClient("mongodb+srv://mongo-skoreddi:Sravani1234@cluster0.xwp4e.mongodb.net/test")
+app.db = client.mflix
+movies = []
 
-@app.route('/')
+@app.route('/',methods = ["GET","POST"])
 def index():
-  return  "<h1>Welcome to Movies Identifier Search Service</h1>"
-##mongodb+srv://mongo-skoreddi:<password>@cluster0.xwp4e.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+
+  return  render_template('templates/admin/index.html')
+
 ##IMDb movies extensive dataset

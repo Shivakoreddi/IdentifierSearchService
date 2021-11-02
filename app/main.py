801fd0ba590aa3b328fb0dd3a1e04f02a13abd54
Shivakoreddi <>
2021-11-01 20:56:46 -0400

    commit

--- app/main.py
+++ app/main.py
@@ -1,5 +1,9 @@
-from flask import Flask
+from flask import Flask,render_template
 app= Flask(__name__)
+
 @app.route('/')
 def index():
-  return "<h1>Welcome to Identifier Search Service</h1>"
+  return  render_template('index.html')
+
+##mongodb+srv://mongo-skoreddi:<password>@cluster0.xwp4e.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+##IMDb movies extensive dataset

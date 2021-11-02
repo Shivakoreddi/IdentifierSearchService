393be074c559b25c3b07e8bcd7c8a667c38eba8f
Shivakoreddi <>
2021-11-02 00:52:23 -0400

    commit

--- app/app.py
+++ app/app.py
@@ -1,9 +1,7 @@
 from flask import Flask,render_template,request
 import pymongo as pm
 app= Flask(__name__)
-client = pm.MongoClient("mongodb+srv://mongo-skoreddi:Sravani1234@cluster0.xwp4e.mongodb.net/test")
-app.db = client.mflix
-movies = []
+
 
 @app.route('/',methods = ["GET","POST"])
 def index():
--- wsgi.py
+++ wsgi.py
@@ -1,3 +1,3 @@
-from app.main import app
+from app.app import app
 if __name__ == "__main__":
   app.run()

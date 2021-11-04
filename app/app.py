8aeeb083a7ce7325e5ef4b7b51f5693a1cdd7e7d
Shivakoreddi <>
2021-11-04 16:44:33 -0400

    Update app.py

--- app/app.py
+++ app/app.py
@@ -5,7 +5,7 @@ from flask import Flask,render_template,request,redirect,url_for,session
 import pymongo as pm
 app= Flask(__name__)
 
-client = pm.MongoClient("mongodb+srv://mongo-skoreddi:Sravani1234@cluster0.xwp4e.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+client = pm.MongoClient("mongodb+srv://mongo-skoreddi:Flask1234@cluster0.xwp4e.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
 app.db = client.mflix
 app.secret_key = 'hello'
 
